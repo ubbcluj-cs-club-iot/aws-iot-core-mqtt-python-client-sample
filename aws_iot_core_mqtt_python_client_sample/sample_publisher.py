c43d8f9381686af8a53f2e7425e806e6c7b8f065
Andrei Olar <andrei.olar@samlex.ro>
2022-11-14 20:30:56 +0200

    Make sample publisher more extensible

--- aws_iot_core_mqtt_python_client_sample/sample_publisher.py
+++ aws_iot_core_mqtt_python_client_sample/sample_publisher.py
@@ -1,21 +1,22 @@
-import click
+"""Publish a message to an MQTT topic"""
 import json
-
-from awscrt import mqtt
-from awsiot import mqtt_connection_builder
 from logging import basicConfig, getLogger, DEBUG
 from os import path
-from random import random
 from time import sleep
 from uuid import uuid4
+
+import click
+from awscrt import mqtt
+from awscrt.exceptions import AwsCrtError
+from awsiot import mqtt_connection_builder
 
 
 # certificate paths will be computed relative to current file dir
 DIR_PATH = path.abspath(path.dirname(__file__))
 
 # names of files that contain the relevant certificates
-DEVICE_CERTIFICATE_FILENAME = "device.pem"
-DEVICE_KEY_FILENAME = "device_rsa"
+DEVICE_CERTIFICATE_FILENAME = "cert.pem"
+DEVICE_KEY_FILENAME = "private.key"
 AWS_ROOT_CA = "AmazonRootCA1.pem"
 
 # setup logger
@@ -26,15 +27,15 @@ LOG = getLogger(__name__)
 def _new_mqtt_connection(
     aws_mqtt_endpoint: str,
     client_id: str,
-    dev_cert_filename: str,
-    dev_key_filename: str,
-    ca_filename: str,
+    dev_pem_path: str,
+    dev_private_key_path: str,
+    root_ca_path: str,
 ):
     return mqtt_connection_builder.mtls_from_path(
         endpoint=aws_mqtt_endpoint,
-        cert_filepath=path.join(DIR_PATH, dev_cert_filename),
-        pri_key_filepath=path.join(DIR_PATH, dev_key_filename),
-        ca_filepath=path.join(DIR_PATH, ca_filename),
+        cert_filepath=dev_pem_path,
+        pri_key_filepath=dev_private_key_path,
+        ca_filepath=root_ca_path,
         client_id=client_id,
         clean_session=True,
         keep_alive_secs=10,
@@ -56,24 +57,39 @@ def _new_mqtt_connection(
     "--topic",
     "-t",
     type=click.STRING,
-    help="Topic to subscribe to",
+    help="Topic to publish to",
     default="test/commands",
 )
 @click.option(
     "--count",
     "-n",
     type=click.INT,
-    help="Number of messages to publish",
-    default=10
+    help="Number of times to publish the message",
+    default=1,
 )
-def cli(endpoint, client_prefix, topic, count):
+@click.option(
+    "--cert-dir",
+    "-C",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+    help="Directory containing AWS IoT Core certificates",
+    default=DIR_PATH,
+)
+@click.option(
+    "--message",
+    "-m",
+    type=click.STRING,
+    help="The actual message you want to publish on the MQTT topic",
+)
+def cli(endpoint, client_prefix, topic, count, cert_dir, message):
+    """Publish a message to an MQTT topic on AWS IoT Core."""
     client_id = f"{client_prefix}-{uuid4()}"
+
     connection = _new_mqtt_connection(
         aws_mqtt_endpoint=endpoint,
         client_id=client_id,
-        dev_cert_filename=DEVICE_CERTIFICATE_FILENAME,
-        dev_key_filename=DEVICE_KEY_FILENAME,
-        ca_filename=AWS_ROOT_CA,
+        dev_pem_path=path.join(cert_dir, DEVICE_CERTIFICATE_FILENAME),
+        dev_private_key_path=path.join(cert_dir, DEVICE_KEY_FILENAME),
+        root_ca_path=path.join(cert_dir, AWS_ROOT_CA),
     )
     LOG.debug(
         "connecting to '%(endpoint)s using client ID '%(client_id)s'",
@@ -83,23 +99,24 @@ def cli(endpoint, client_prefix, topic, count):
         future = connection.connect()
         future.result()
         LOG.info("connected")
+    except AwsCrtError as ace:
+        LOG.fatal("could not connect to AWS IoT Core", exc_info=ace)
+        return
 
-        for i in range(1, count+1):
-            rand = random()
-            temperature = 23.0 - rand if (i % 2) == 0 else 23.0 + rand
-            humidity = 150.0 + 3.14 * rand
+    try:
+        for i in range(1, count + 1):
             connection.publish(
                 topic=topic,
-                payload=json.dumps({"temperature": temperature, "humidity": humidity}),
+                payload=message,
                 qos=mqtt.QoS.AT_LEAST_ONCE,  # quality of service
             )
             LOG.debug(
-                "published message #%(index)d on '%(topic)s",
+                "published message #%(index)d on '%(topic)s'",
                 {"index": i, "topic": topic},
             )
             sleep(1)
-    except Exception as e:
-        LOG.fatal("AWS IoT publish failed!", exc_info=e)
+    except Exception as exc:  # pylint: disable=broad-except
+        LOG.fatal("Error while p", exc_info=exc)
     finally:
         LOG.info("done")
 
